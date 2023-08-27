1191d376c5241f38d774b7b209d64bc85a681684
Michael Kenny <git@mmkenny.net>
2023-08-27 13:37:12 +0930

    Added 2 second delay to setting resolution in OnStreamStart.
    This is needed for my setup with MonitorSwapper.

--- ResolutionMatcher-Functions.ps1
+++ ResolutionMatcher-Functions.ps1
@@ -105,6 +105,7 @@ function Stop-ResolutionMatcherScript() {
 
 function OnStreamStart($width, $height, $refresh) {
     $expectedRes = Join-Overrides -width $width -height $height -refresh $refresh
+    Start-Sleep -Seconds 2
     Set-ScreenResolution -Width $expectedRes.Width -Height $expectedRes.Height -Freq $expectedRes.Refresh
 }
 
